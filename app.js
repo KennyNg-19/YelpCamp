e71b0a175dc5c77eeb0a420e4ff13c964898f3aa
YuhaoWU <wuyuhao2019@126.com>
2023-03-10 11:58:27 +0800

    edit mongoose 丢弃的

--- app.js
+++ app.js
@@ -1,67 +1,79 @@
+/*
+ * @Author: YuhaoWU
+ * @Date: 2023-01-30 16:14:40
+ * @LastEditors: YuhaoWU
+ * @LastEditTime: 2023-03-10 11:47:39
+ * @Description:
+ */
 // Requiring modules
-var express             = require("express"),
-    app                 = express(),
-    bodyParser          = require("body-parser"),
-    mongoose            = require("mongoose"),
-    passport            = require("passport"),
-    LocalStrategy       = require("passport-local"),
-    methodOverride      = require("method-override"),
-    flash               = require("connect-flash"),
-    cookieParser        = require("cookie-parser"),
-    session             = require("express-session"),
-    // load collections
-    Campground          = require("./models/campground"),
-    Comment             = require("./models/comment"),
-    User                = require("./models/user");
-
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  flash = require("connect-flash"),
+  cookieParser = require("cookie-parser"),
+  session = require("express-session"),
+  // load collections
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user");
 
 // Requiring routes
-var commentRoutes       = require("./routes/comments"),
-    campgroundRoutes    = require("./routes/campgrounds"),
-    indexRoutes         = require("./routes/index");
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
 // Assigning mongoose promise library and connecting to db
 mongoose.Promise = global.Promise;
-const databaseUri = process.env.MONGODB_URI || "mongodb+srv://admin-kenny:1@cluster0-7phca.mongodb.net/yelp-campDB";
-mongoose.connect(databaseUri, { useMongoClient: true })
-      .then(() => console.log(`Database connected`))
-      .catch(err => console.log(`Database connection error: ${err.message}`));
+const databaseUri =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://admin-kenny:1@cluster0-7phca.mongodb.net/yelp-campDB";
+mongoose
+  .connect(databaseUri, {
+    // useMongoClient: true
+    useNewUrlParser: true,
+  })
+  .then(() => console.log(`Database connected`))
+  .catch((err) => console.log(`Database connection error: ${err.message}`));
 
 // Accounting for deprecated mongoose settings
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-app.use(bodyParser.urlencoded({extended: true}));
-
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
-app.use(cookieParser('secret'));
+app.use(cookieParser("secret"));
 
 // Requiring moment
 app.locals.moment = require("moment");
 
 // Passport configuration
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "Any string can go here!",
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 // Setting up flash messages
 app.use(flash());
-app.use(function(req, res, next){
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
 });
 
 // Setting route prefixes so we don't have to type them out in /routes
@@ -73,6 +85,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Server started on port " + port);
 });
